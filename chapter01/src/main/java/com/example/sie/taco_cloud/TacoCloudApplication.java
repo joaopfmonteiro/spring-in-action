21f2899aaf2b5e559be69593ce470786d00ec83f
joaopfmonteiro <joaoosverdes@gmail.com>
2025-07-07 15:23:04 +0200

    @SpringBootApplication annotation

--- chapter01/src/main/java/com/example/sie/taco_cloud/TacoCloudApplication.java
+++ chapter01/src/main/java/com/example/sie/taco_cloud/TacoCloudApplication.java
@@ -3,9 +3,19 @@ package com.example.sie.taco_cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SpringBoot application
+//@SpringBootApplication annotation clearly signifies that this is a Spring Boot application
+
+//@SpringBootApplication is a composite annotation that combines the following
+//three annotations:
+//@SpringBootConfiguration—Designates this class as a configuration class.
+//@EnableAutoConfiguration—Enables Spring Boot automatic configuration.
+//@ComponentScan—Enables component scanning.
+//application context.
 @SpringBootApplication
 public class TacoCloudApplication {
 
+	//Runs the application
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
